7b022e5f93426a0a07f6f57388d515c424b94450
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-02-20 20:06:09 -0300

    feat: criado user factory

---
+++ register-service/src/domain/user/factory/user.factory.ts
@@ -0,0 +1,15 @@
+import { v4 as uuidv4 } from 'uuid';
+import Address from "../../address/entity/address";
+import User from "../entity/user";
+
+class UserFactory {
+    static create(name: string, email: string, cpf: string): User {
+        return User.createUser(name, email, cpf);
+    }
+
+    static createWithAddress(name: string, email: string, cpf: string, address: Address): User {
+        const user = User.createUser(name, email, cpf);
+        user.addAddress(address);
+        return user;
+    }
+}
