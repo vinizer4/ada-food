18b73c8828add306aa886897e692773f0db67d97
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-02-21 01:30:32 -0300

    feat: criado endpoints para usecases update user find user e delete user

--- register-service/src/config/init/initializer.ts
+++ register-service/src/config/init/initializer.ts
@@ -1,9 +1,17 @@
 import {UserRepositoryAdapter} from "../adapters/db/database.adapter";
 import {CreateUserUsecase} from "../../core/usecase/user/create/create.user.usecase";
+import {UpdateUserUsecase} from "../../core/usecase/user/update/update.user.usecase";
+import {DeleteUserUseCase} from "../../core/usecase/user/delete/delete.user.usecase";
+import {FindUserByIdUseCase} from "../../core/usecase/user/find/find.user.by.id.usecase";
+import {FindUserByEmailUseCase} from "../../core/usecase/user/find/find.user.by.email.usecase";
 
 export class Initializer {
     static initialize() {
         UserRepositoryAdapter.createUserRepository();
         CreateUserUsecase.getInstance(UserRepositoryAdapter.getUserRepository());
+        UpdateUserUsecase.getInstance(UserRepositoryAdapter.getUserRepository());
+        DeleteUserUseCase.getInstance(UserRepositoryAdapter.getUserRepository());
+        FindUserByIdUseCase.getInstance(UserRepositoryAdapter.getUserRepository());
+        FindUserByEmailUseCase.getInstance(UserRepositoryAdapter.getUserRepository());
     }
 }
