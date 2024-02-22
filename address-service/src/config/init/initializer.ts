96894a00aaec96533f5ee1f006b209923a21b7cc
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-02-21 22:28:29 -0300

    feat: finalizado address service

--- address-service/src/config/init/initializer.ts
+++ address-service/src/config/init/initializer.ts
@@ -1,7 +1,15 @@
 import {DatabaseAdapter} from "../adapters/db/database.adapter";
+import {CreateAddressUsecase} from "../../core/application/usecase/address/create/create.address.usecase";
+import {UpdateAddressUsecase} from "../../core/application/usecase/address/update/update.address.usecase";
+import {DeleteAdressUsecase} from "../../core/application/usecase/address/delete/delete.address.usecase";
+import {FindAddressByUserUsecase} from "../../core/application/usecase/address/find/find.address.by.user.usecase";
 
 export class Initializer {
     static async initialize() {
         DatabaseAdapter.createAddressRepository();
+        CreateAddressUsecase.getInstance(DatabaseAdapter.getAddressRepository());
+        UpdateAddressUsecase.getInstance(DatabaseAdapter.getAddressRepository());
+        DeleteAdressUsecase.getInstance(DatabaseAdapter.getAddressRepository());
+        FindAddressByUserUsecase.getInstance(DatabaseAdapter.getAddressRepository());
     }
 }
