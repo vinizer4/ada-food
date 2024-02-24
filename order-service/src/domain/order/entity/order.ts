8d88be4656d5d229e264385d0f0e0d2d94053b01
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-02-24 16:52:50 -0300

    feat: criado config mongodb e model order

--- order-service/src/domain/order/entity/order.ts
+++ order-service/src/domain/order/entity/order.ts
@@ -2,14 +2,16 @@ export class Order {
     private id: string;
     private userId: string;
     private addressId: string;
+    private description: string;
 
-    private constructor(id: string, userId: string, addressId: string) {
+    private constructor(id: string, userId: string, addressId: string, description: string) {
         this.id = id;
         this.userId = userId;
         this.addressId = addressId;
+        this.description = description;
     }
 
-    static create(id: string, userId: string, addressId: string) {
-        return new Order(id, userId, addressId);
+    static create(id: string, userId: string, addressId: string, description: string) {
+        return new Order(id, userId, addressId, description);
     }
 }
--- order-service/src/domain/order/factory/order.factory.ts
+++ order-service/src/domain/order/factory/order.factory.ts
@@ -1,8 +1,8 @@
 import {Order} from "../entity/order";
 
 export class OrderFactory {
-    static create(id: string, userId: string, addressId: string) {
-        const order = Order.create(id, userId, addressId);
+    static create(id: string, userId: string, addressId: string, description: string) {
+        const order = Order.create(id, userId, addressId, description);
         return order;
     }
 }
