8841b6fb9ea24412646b87fea6e2370832340a40
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-02-25 16:01:45 -0300

    feat: criado verify token usecase auth-service

---
+++ auth-service/src/core/domain/usecases/verif-token/verify.token.use.case.ts
@@ -0,0 +1,40 @@
+import {TokenService} from "../../service/token/token.service";
+import {AuthenticationException} from "../../../exception/authentication.exception";
+
+export class VerifyTokenUseCase {
+    private static instance: VerifyTokenUseCase;
+
+    private tokenService: TokenService;
+
+    private constructor(
+        tokenService: TokenService
+    ) {
+        this.tokenService = tokenService;
+    }
+
+    public static getInstance(
+        tokenService: TokenService
+    ): VerifyTokenUseCase {
+        if (!VerifyTokenUseCase.instance) {
+            VerifyTokenUseCase.instance =
+                new VerifyTokenUseCase(
+                    tokenService
+                );
+        }
+        return VerifyTokenUseCase.instance;
+    }
+
+    async execute(token: string): Promise<boolean> {
+        try {
+            return this.tokenService.verifyToken(token);
+        }
+        catch (error: any) {
+            throw this.exceptionHandler(error);
+        }
+    }
+
+    private exceptionHandler(error: any) {
+        console.error("[VerifyTokenUseCase] - Erro ao verificar token: ", error);
+        throw new AuthenticationException("Erro ao verificar token");
+    }
+}
